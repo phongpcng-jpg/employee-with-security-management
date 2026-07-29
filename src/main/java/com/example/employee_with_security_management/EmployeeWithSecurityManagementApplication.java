22eb42068537f49b0f58fedc985a70fea310581e
Thomas Phong <ino.thomas@outlook.com>
2026-07-29 09:54:06 +0700

    first commit

---
+++ src/main/java/com/example/employee_with_security_management/EmployeeWithSecurityManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_with_security_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWithSecurityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWithSecurityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employee_with_security_management/EmployeeWithSecurityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employee_with_security_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWithSecurityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
